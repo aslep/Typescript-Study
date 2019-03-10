65e1d0c9594f72c5c53451a5410ca4cc053e0518
aslep <aslep_@hotmail.com>
2019-03-10 18:13:34 +0800

    解决vscode环境下的typescript版本坑

---
+++ firstWeek/index.js
@@ -0,0 +1,11 @@
+/**
+ * 模板字符串使用
+ * 这种字符串是被反引号包围(`)，并且以${ expr }这种形式嵌入表达式
+ */
+var myName = "helen";
+var age = 18;
+var sentence = "Hello~!My name is " + myName + ".I'll be " + (age + 1) + " years old next year.";
+console.log(sentence);
+/**
+ * 创建一个长度为5的数组，并对其进行增删改，最后获取数组中的第三个元素
+ */
